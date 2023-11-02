4a2633183fab63ff5410aa81da4828af119c61c5
bulat <bulatxajrutdinoff@yandex.ru>
2023-11-02 19:42:18 +0300

    1. Добавляет приложение в настройки 2. Создает шаблон lists/template/home.html 3. Возвращает в представлении шаблон home.html для / 4. В модульном тесте использует Тестового клиента Джанго для проверки, что главная страница использует правильный шаблон

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page(request):
-    html = "<html>"
-    html += "<title>To-Do lists</title>"
-    html += "</html>"
-    return HttpResponse(html)
+    return render(request, "home.html")
