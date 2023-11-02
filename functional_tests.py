d4dce55bd5d6a03f2a5b844119bf6a4bc2314418
bulat <bulatxajrutdinoff@yandex.ru>
2023-11-02 18:38:53 +0300

    Представление возвращает минимальный HTML с помощью HttpResponse

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn("To-Do", self.browser.title)
-        # self.fail("Закончить тест!")
+        self.fail("Закончить тест!")
 
         # Ей сразу же предлагается ввести элемент списка
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_page():
-    pass
+
+
+def home_page(request):
+    html = "<html>"
+    html += "<title>To-Do lists</title>"
+    html += "</html>"
+    return HttpResponse(html)
