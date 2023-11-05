6aec6619a22a9bdd242ef9a1615a62146646d5ec
bulat <bulatxajrutdinoff@yandex.ru>
2023-11-05 21:13:11 +0400

    Тест проверяет возможность сохранить 2 задачи и вывести их внутри таблицы

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,12 @@ class NewVisitorTest(unittest.TestCase):
         """демонтаж"""
         pass
 
+    def check_for_row_in_list_table(self, row_text):
+        """подтверждение строки в таблице списка"""
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
         # Эдит слышала про крутое новое онлайн приложение со списком
@@ -41,24 +47,21 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-
-        # self.assertTrue(
-        #     any(row.text == "1: Купить павлиньи перья" for row in rows),
-        #     f"Новый элемент списка не появился в таблице.
-        #     Содержимое было:\n{table.text}",
-        # )
-        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-        self.fail("Закончить тест!")
+        inputbox = self.browser.find_element(value="id_new_item")
+        inputbox.send_keys("Сделать мушку из павлиньих перьев")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
 
+        self.fail("Закончить тест!")
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес - об этом
         # выводится небольшой текст с объяснениями.
