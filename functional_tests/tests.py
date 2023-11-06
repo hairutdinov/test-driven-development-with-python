cba65ded99c90ddbdc10e69ef7657106fee3fc19
bulat <bulatxajrutdinoff@yandex.ru>
2023-11-06 12:24:59 +0400

    1. Модуль functional_tests 2. ФТ использует django.test.LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 import time
-import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """Тест нового посетителя"""
 
     def setUp(self):
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         """тест: можно начать список и получить его позже"""
         # Эдит слышала про крутое новое онлайн приложение со списком
         # неотложных дел. Она решает оценить его домашнюю страницу
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
@@ -69,7 +69,3 @@ class NewVisitorTest(unittest.TestCase):
         # Она посещает этот URL-адрес - ее список по-прежнему там.
 
         # Удовлетворенная, она снова ложится спать
-
-
-if __name__ == "__main__":
-    unittest.main()
